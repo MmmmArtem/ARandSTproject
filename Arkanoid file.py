e1b52ff9f35aae1b0d0e5bb1793821934ca61b34
ArtemMm <artemus.smir@gmail.com>
2020-01-25 12:21:27 +0300

    Shar + schet

--- Arkanoid file.py
+++ Arkanoid file.py
@@ -114,3 +114,103 @@ class Platform(pygame.sprite.Sprite):
             else:
                 self.moverght = False
 
+
+class Shar(pygame.sprite.Sprite):
+    image = load_image('shar.png')
+
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.image = Shar.image
+        self.rect = self.image.get_rect()
+        self.dx = 0
+        self.dy = 0
+        self.vx = game_speed_x0 * game_speed
+        self.vy = game_speed_y0 * game_speed
+        self.numx = 1
+        self.numy = 1
+        self.razbit_kirp = 0
+
+    def reset(self):
+        self.rect.y = 300
+        self.rect.x = 300
+        self.dx = 0
+        self.dy = 0
+        self.vx = game_speed_x0 * game_speed
+        self.vy = game_speed_y0 * game_speed
+        self.numx = 1
+        self.numy = 1
+
+    def update(self, dt):
+        global lose, game_live, next_live
+        self.dx += self.vx * dt
+        self.dy += self.vy * dt
+        int_dx = int(self.dx)
+        int_dy = int(self.dy)
+        if int_dx > 0:
+            self.rect.x += int_dx * self.numx
+            # отскок от стен
+            if self.rect.x <= 0 or self.rect.x + self.rect.width > width:
+                self.numx = -self.numx
+            self.dx = self.dx - int_dx
+        if int_dy > 0:
+            self.rect.y += int_dy * self.numy
+            if self.rect.y + self.rect.height >= height:
+                if god_mode:
+                    self.numy = -self.numy
+                    self.rect.y = height - self.rect.height
+                else:
+                    game_live -= 1
+                    # игра проиграна
+                    if game_live == 0:
+                        pygame.mixer.music.stop()
+                        lose = True
+                    else:
+                        next_live = True
+            # отскок от потолка
+            if self.rect.y <= 0:
+                self.numy = -self.numy
+                self.rect.y = 0
+            self.dy = self.dy - int_dy
+        # отскок от платформы
+        if not lose:
+            coll_p = pygame.sprite.spritecollide(self, platforms, False)
+            if coll_p:
+                for plat in coll_p:
+                    sb = sboku(plat.rect, self.rect, self.numy)
+                    if sb == 'left':
+                        self.numx = -self.numx
+                    elif sb == 'right':
+                        self.numx = -self.numx
+                    else:
+                        # Подкручивание
+                        if plat.moveleft and self.numx > 0:
+                            self.numx = -self.numx
+                        if plat.moverght and self.numx < 0:
+                            self.numx = -self.numx
+                        # Шар выше платформы
+                        if self.numy > 0:
+                            self.numy = -self.numy
+                            self.rect.y = plat.rect.y - self.rect.height
+                        else:
+                            self.numy = -self.numy
+                            self.rect.y = plat.rect.y + self.rect.height
+            coll_k = pygame.sprite.spritecollide(self, kirpichs, False)
+            if coll_k:
+                for kirp in coll_k:
+                    lk = 0
+                    rk = 0
+                    sb = sboku(kirp.rect, self.rect, self.numy)
+                    if sb == 'left':
+                        if lk == 0:
+                            self.numx = -self.numx
+                        lk += 1
+                    elif sb == 'right':
+                        if rk == 0:
+                            self.numx = -self.numx
+                        rk += 1
+                    else:
+                        self.numy = -self.numy
+                    kirp.kontakt += 1
+                    if kirp.kontakt == game_level:
+                        self.razbit_kirp += game_level
+                        kirp.kill()
